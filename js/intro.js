5ce78df5d8df9e20b85017a0585f755c5f5e3568
PranishShresth <yunche64@gmail.com>
2021-08-24 19:21:48 +1000

    arrays

--- js/intro.js
+++ js/intro.js
@@ -7,8 +7,43 @@ console.log("Hello world");
 // Compositive type
 // arrays, object
 
+// String
 var string = "This is a string";
 console.log(string);
+
+// Number
 var number = 1000;
 var number1 = 1000.2323;
 console.log(number, number1);
+
+// Boolean
+var sayYes = true;
+var sayNo = false;
+console.log(sayYes, sayNo);
+
+// Undefined
+var jeremy;
+
+// Null
+var lucian = null;
+
+// Symbol
+let sym = Symbol();
+
+console.log(jeremy);
+
+console.log(lucian);
+console.log(sym);
+
+// Compositive
+var arrays = [
+  "Lucian",
+  "Jeremy",
+  23,
+  1002,
+  null,
+  undefined,
+  Symbol(),
+  ["Hello world"],
+];
+console.log(arrays);
