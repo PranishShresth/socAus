a3c8c193df78139b5c52e344643270712a6f53f8
PranishShresth <yunche64@gmail.com>
2021-08-24 19:13:45 +1000

    js

--- js/intro.js
+++ js/intro.js
@@ -1,1 +1,14 @@
 console.log("Hello world");
+
+// Data types
+// String, Number, Boolean, null, undefined , symbol, arrays, object
+// Primitive type
+// String, Number, Boolean, null, undefined , symbol,
+// Compositive type
+// arrays, object
+
+var string = "This is a string";
+console.log(string);
+var number = 1000;
+var number1 = 1000.2323;
+console.log(number, number1);
