8663f91f4f1afea6d23078bacd34e4223e6e4749
PranishShresth <yunche64@gmail.com>
2021-08-25 18:19:04 +1000

    loops

---
+++ js/loop.js
@@ -0,0 +1,38 @@
+var i = 10;
+for (; i < 1000; i += 20) {
+  console.log(i);
+  console.log("jeremy ");
+}
+
+for (var i = 0; i < 100; ++i) {
+  if (i === 50) {
+    continue;
+  }
+  console.log(i);
+}
+
+var number = 50;
+
+while (number <= 100) {
+  console.log(number);
+  number++;
+}
+
+var i = 1;
+while (true) {
+  if (i === 1000) {
+    break;
+  }
+  console.log(i);
+  i++;
+}
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
