33c6c389b990db8222f051bddf4cc105fb1b400a
PranishShresth <yunche64@gmail.com>
2021-08-25 19:45:34 +1000

    passed by value/reference

---
+++ js/hoisting.js
@@ -0,0 +1,26 @@
+console.log(number);
+var number;
+number = 20;
+
+helloWorld();
+
+function helloWorld() {
+  console.log("hello world");
+}
+
+////var number;
+//var fn;
+// fn();
+// function helloWorld() {
+//   console.log("hello world");
+// }
+
+//number = 20;
+// console.log(number);
+//
+
+// helloWorld();
+
+const fn = function () {
+  console.log("hello world");
+};
