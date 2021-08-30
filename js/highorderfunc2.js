a902c9bd53b713fc468a74995edf075eb444e52e
PranishShresth <yunche64@gmail.com>
2021-08-30 18:47:29 +1000

    every,some,find and findIndex

--- js/highorderfunc2.js
+++ js/highorderfunc2.js
@@ -22,3 +22,53 @@ const age = person.reduce((ages, human) => {
   return ages + human.age;
 }, 0);
 console.log(age);
+
+// every
+
+const strings = ["Jeremy", "Viqaas", "Pranish", "Luke", "Lucian", 23];
+
+const isString1 = strings.every((personName) => {
+  return typeof personName === "string";
+});
+
+const isString2 = strings.some((personName) => {
+  return typeof personName === "number";
+});
+console.log(isString1, "every");
+console.log(isString2, "some");
+
+function isString(strings) {
+  for (let i = 0; i < strings.length; i++) {
+    if (typeof strings[i] !== "string") {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isString(strings));
+
+const movie = ["dory", "nemo", "marlan", "nemo", "bruce"];
+
+const findingNemo = movie.find((character) => {
+  return character === "nemo";
+});
+
+const findingNemoPosition = movie.findIndex((character) => {
+  return character === "nemo";
+});
+console.log(findingNemo);
+console.log(findingNemoPosition);
+
+function map(cb, arr) {
+  const arry = [];
+  for (let i = 0; i < arr.length; i++) {
+    arry.push(cb(arr[i], i, arr));
+    console.log("hello");
+  }
+  return arry;
+}
+const hi = map((ele) => {
+  return "hi" + ele;
+}, movie);
+console.log(hi);
