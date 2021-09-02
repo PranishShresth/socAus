804c50545afe62c3f3a5a14e366465376b3ae522
PranishShresth <yunche64@gmail.com>
2021-09-02 19:51:38 +1000

    deleteBtn

--- JSdom/shoppingList/static/script.js
+++ JSdom/shoppingList/static/script.js
@@ -1,9 +1,21 @@
 let form = document.querySelector("form");
-
+let shoppingList = document.querySelector(".shoppingList");
 form.addEventListener("submit", function (ev) {
   // prevents page from reloading
   ev.preventDefault();
+  //   accessing the input using ev.target
   const shoppingItem = ev.target[0].value;
-  console.log(ev);
-  console.log(shoppingItem);
+
+  if (shoppingItem === "") {
+    return alert("The shopping item shouldn't be empty");
+  }
+
+  //   creating a div element
+  let div = document.createElement("div");
+  //   adding a class
+  div.classList.add("shopping_item");
+
+  let item = `<p>${shoppingItem}</p><p class="deleteBtn">Delete</p>`;
+  div.innerHTML = item;
+  shoppingList.appendChild(div);
 });
