0bd7332ec4a80f6f6cf49d2424885718c9718dc4
PranishShresth <yunche64@gmail.com>
2021-09-03 19:02:42 +1000

    dimensions oval project

---
+++ JSdom/api/index.js
@@ -0,0 +1,11 @@
+console.log("hello");
+console.log("hi");
+
+setTimeout(() => {
+  console.log("after 5 seconds");
+}, 5000);
+
+setInterval(() => {
+  console.log("every 2 seconds");
+}, 2000);
+console.log("hey");
