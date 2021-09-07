394f7ed1ee946615973a7d6be18e002ff94e67cb
PranishShresth <yunche64@gmail.com>
2021-09-07 19:08:16 +1000

    weather card

--- JSdom/weatherapp/statics/script.js
+++ JSdom/weatherapp/statics/script.js
@@ -6,6 +6,29 @@
 
 // getPosition().then((pos) => console.log(pos));
 
+function generateWeatherCard(weather) {
+  return `
+    <div class="weatherCard">
+      <div class="city_name">${weather.name}</div>
+      <div class="temperature">${weather.main.temp}</div>
+      <div class="description">${weather.weather[0].main}</div>
+      <div class="weather_infos">
+        <div class="weather_info">
+          <div class="weather_heading">Humidity</div>
+          <div class="weather_info_data">${weather.main.humidity}</div>
+        </div>
+        <div class="weather_info">
+          <div class="weather_heading">Pressure</div>
+          <div class="weather_info_data">${weather.main.pressure}</div>
+        </div>
+        <div class="weather_info">
+          <div class="weather_heading">Sea Level</div>
+          <div class="weather_info_data">${weather.main.sea_level}</div>
+        </div>
+      </div>
+    </div>`;
+}
+
 function getWeatherByCoords(latitude, longitude) {
   const API_KEY = "675b4f3777298bb6d7d29c4deee1d463";
   const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`;
