4f6fc9f6f47c352528a91d880fdbf136e302cb46
PranishShresth <yunche64@gmail.com>
2021-09-07 18:51:02 +1000

    weather info

--- JSdom/weatherapp/statics/script.js
+++ JSdom/weatherapp/statics/script.js
@@ -6,7 +6,11 @@
 
 // getPosition().then((pos) => console.log(pos));
 
-function getWeatherByCoords(latitude, longitude) {}
+function getWeatherByCoords(latitude, longitude) {
+  const API_KEY = "675b4f3777298bb6d7d29c4deee1d463";
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`;
+  axios.get(API_URL).then((result) => console.log(result.data));
+}
 
 function main() {
   navigator.geolocation.getCurrentPosition(function (position) {
