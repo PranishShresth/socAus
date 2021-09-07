f415a7cd262dbe385c1f3dccbdaedc46d6227e1b
PranishShresth <yunche64@gmail.com>
2021-09-07 20:01:16 +1000

    weather app

--- JSdom/weatherapp/statics/script.js
+++ JSdom/weatherapp/statics/script.js
@@ -10,6 +10,11 @@ function generateWeatherCard(weather) {
   return `
     <div class="weatherCard">
       <div class="city_name">${weather.name}</div>
+      <img src="http://openweathermap.org/img/wn/${
+        weather.weather[0].icon
+      }@2x.png" alt=${
+    weather.weather[0].main
+  } width="150px" height="150px" style="margin:0 auto"/>
       <div class="temperature">${weather.main.temp}</div>
       <div class="description">${weather.weather[0].main}</div>
       <div class="weather_infos">
@@ -23,23 +28,44 @@ function generateWeatherCard(weather) {
         </div>
         <div class="weather_info">
           <div class="weather_heading">Sea Level</div>
-          <div class="weather_info_data">${weather.main.sea_level}</div>
+          <div class="weather_info_data">${
+            weather.main.sea_level || "N/A"
+          }</div>
         </div>
       </div>
     </div>`;
 }
 
+function getWeatherByCity(city) {
+  const API_KEY = "675b4f3777298bb6d7d29c4deee1d463";
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+  axios.get(API_URL).then((result) => {
+    weatherContainer.innerHTML = generateWeatherCard(result.data);
+  });
+}
+
 function getWeatherByCoords(latitude, longitude) {
+  const weatherContainer = document.querySelector("#weatherContainer");
   const API_KEY = "675b4f3777298bb6d7d29c4deee1d463";
   const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`;
-  axios.get(API_URL).then((result) => console.log(result.data));
+  axios.get(API_URL).then((result) => {
+    weatherContainer.innerHTML = generateWeatherCard(result.data);
+  });
 }
 
 function main() {
-  navigator.geolocation.getCurrentPosition(function (position) {
-    let coords = position.coords;
-    const { latitude, longitude } = coords;
-    getWeatherByCoords(latitude, longitude);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      let coords = position.coords;
+      const { latitude, longitude } = coords;
+      getWeatherByCoords(latitude, longitude);
+    });
+  }
+
+  document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    let city = event.target[0].value;
+    getWeatherByCity(city);
   });
 }
 
