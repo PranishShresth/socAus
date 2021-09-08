7164e5164506b8415f92eb4df1103f8a09ecdab7
PranishShresth <yunche64@gmail.com>
2021-09-08 19:13:49 +1000

    error handler

--- JSdom/githubApp/static/script.js
+++ JSdom/githubApp/static/script.js
@@ -1,16 +1,33 @@
-function main() {
-  let searchForm = document.querySelector(".searchForm");
-  searchForm.onsubmit = function (ev) {
-    ev.preventDefault();
-    console.info("here");
-  };
+function displayError(error) {
+  let hide = document.querySelector(".hide");
+  hide.innerText = error;
+  hide.classList.add("show");
+  setTimeout(() => {
+    hide.classList.remove("show");
+  }, 5000);
 }
 
 function getUserProfile(user) {
   const API_URL = `https://api.github.com/users/${user}`;
-  axios.get(API_URL).then((result) => {
-    consol.log(result.data);
-  });
+  axios
+    .get(API_URL)
+    .then((result) => {
+      console.log(result.data);
+    })
+    .catch(() => displayError("Something went wrong! Try again"));
+}
+
+function main() {
+  let searchForm = document.querySelector(".searchForm");
+  searchForm.onsubmit = function (ev) {
+    ev.preventDefault();
+    let userName = ev.target[0].value;
+    if (userName === "") {
+      displayError("Username shouldn't be empty!");
+      return;
+    }
+    getUserProfile(userName);
+  };
 }
 
 main();
